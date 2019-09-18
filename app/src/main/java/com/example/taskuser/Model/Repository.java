762df853572bc9faad20b674c934fa5cf81cd481
Mahdi.A <ndt.mahdi@gmail.com>
2019-09-18 11:00:47 +0330

    Edit Repository && User class

--- app/src/main/java/com/example/taskuser/Model/Repository.java
+++ app/src/main/java/com/example/taskuser/Model/Repository.java
@@ -7,7 +7,9 @@ import java.util.UUID;
 public class Repository {
     private static Repository ourInstance;
     private List<User> userList;
-   // private List<Task> taskList;
+    private List<Task> taskList;
+
+
     public static Repository getInstance() {
         if(ourInstance==null)
             ourInstance=new Repository();
@@ -49,6 +51,63 @@ public class Repository {
         return false;
     }
 
+    ////////// Insert , Edit , Delete task's method
+
+    public void setTaskList(List<Task> taskList) {
+        this.taskList = taskList;
+    }
+
+    public Task getTask(UUID idTask)
+    {
+        for(Task task:taskList)
+            if (task.getTaskID().equals(idTask))
+                return task;
+            return null;
+    }
+
+    public List<Task> getTaskList(UUID userId)
+    {
+        for (User user:userList)
+            if(user.getUserId().equals(userId))
+                return user.getTaskList();
+            return null;
+    }
+    public void insertTask(Task task)
+    {
+        taskList.add(task);
+    }
+
+    public void editTask(Task task) throws Exception {
+        Task newTask = getTask(task.getTaskID());
+        if (newTask == null)
+            throw new Exception("This Task does not exist!!!");
+
+        newTask.setTitle(task.getTitle());
+        newTask.setDateTask(task.getDateTask());
+        newTask.setDescription(task.getDescription());
+        newTask.setState(task.getState());
+        newTask.setTimeTask(task.getTimeTask());
+        newTask.setIconState(task.getIconState());
+    }
+
+    public void deleteTask(Task Task) throws Exception {
+        Task oldTask = getTask(Task.getTaskID());
+        if (oldTask == null)
+            throw new Exception("This Task does not exist!!!");
+
+        taskList.remove(oldTask);
+    }
+    public int getPositionTask(UUID uuid) {
+        return taskList.indexOf(getTask(uuid));
+    }
+
+    public Task getTask(UUID uuid) {
+        for (Task task : taskList)
+            if (task.getTaskID().equals(uuid))
+                return task;
+
+        return null;
+    }
     private Repository() {
             userList = new ArrayList<>();
     }
--- app/src/main/java/com/example/taskuser/Model/User.java
+++ app/src/main/java/com/example/taskuser/Model/User.java
@@ -1,11 +1,10 @@
 package com.example.taskuser.Model;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 public class User {
-    private UUID userId;
+    //private UUID userId;
     private String userName;
     private int password;
     private List<Task> taskList;
@@ -51,42 +50,5 @@ public class User {
     }
 
 
-////////// Insert , Edit , Delete task's method
 
-    public void insertTask(Task task)
-    {
-        taskList.add(task);
-    }
-
-    public void editTask(Task task) throws Exception {
-        Task newTask = getTask(task.getTaskID());
-        if (newTask == null)
-            throw new Exception("This Task does not exist!!!");
-
-        newTask.setTitle(task.getTitle());
-        newTask.setDateTask(task.getDateTask());
-        newTask.setDescription(task.getDescription());
-        newTask.setState(task.getState());
-        newTask.setTimeTask(task.getTimeTask());
-        newTask.setIconState(task.getIconState());
-    }
-
-    public void deleteTask(Task Task) throws Exception {
-        Task oldTask = getTask(Task.getTaskID());
-        if (oldTask == null)
-            throw new Exception("This Task does not exist!!!");
-
-        taskList.remove(oldTask);
-    }
-    public int getPositionTask(UUID uuid) {
-        return taskList.indexOf(getTask(uuid));
-    }
-
-    public Task getTask(UUID uuid) {
-        for (Task task : taskList)
-            if (task.getTaskID().equals(uuid))
-                return task;
-
-        return null;
-    }
 }
